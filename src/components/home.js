e9951351ce8ee128198cea6cd19bfedfdf7d0da2
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-07-13 15:25:56 -0700

    added covid data widget to home page

--- src/components/home.js
+++ src/components/home.js
@@ -4,9 +4,11 @@ function Home () {
     return(
         <div className="home-container">
             <h1 className="home-item"><strong>Covid Conscious</strong></h1>
-            <h3 className="home-item">Keep a log of places been and people seen</h3>
-            
+            <h3 className="home-item">Keep a track of places been and people seen</h3>
+            <div class="bingwidget offset-xl-6 offset-lg-5 offset-md-4 offset-sm-1" data-type="covid19_stats_map" data-market="en-us" data-language="en-us" data-app="bingwidget"></div>
         </div>
+        
+        
     )
 }
 
