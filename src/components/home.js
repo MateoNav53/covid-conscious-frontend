73acd0df8694e651a81334e0f7f9426b7c4fe0d1
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-07-12 19:57:07 -0700

    started home page

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home () {
+    return(
+        <div className="home-container">
+            <h1 className="home-item"><strong>Covid Conscious</strong></h1>
+            <h3 className="home-item">Keep a log of places been and people seen</h3>
+            
+        </div>
+    )
+}
+
+export default Home;
