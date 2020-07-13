bab41f1f7819efe0f0bb3ddc784072e4aaa6eca9
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-07-12 22:04:12 -0700

    tied all input fields to state

--- src/components/addLog.js
+++ src/components/addLog.js
@@ -6,28 +6,46 @@ function AddLog() {
     
     const [logLocation, setLogLocation] = useState('')
     const [logDate, setLogDate] = useState(null)
-    // const [logDuration, setLogDuration] = useState(null)
-    // const [logInteraction, setLogInteraction] = useState(null)
+    // const [logItem, setLogItem] = useState()
+    const [logDuration, setLogDuration] = useState('')
+    const [logInteractions, setLogInteractions] = useState('')
+
+    // function logData() {
+    //     console.log(logDate)
+    // }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setLogLocation(e.target.value)
+        setLogDate(e.target.value)
+        setLogDuration(e.target.value)
+        setLogInteractions(e.target.value)
+        console.log(logLocation)
+        console.log(logDate)
+        console.log(logDuration)
+        console.log(logInteractions)
+        e.target.reset()
+    }
 
     return(
         <div>
-            <form className="col-md-8 offset-md-2">
-                <h1 className="form-group add-log-header">Record your physical interactions and activities</h1>
+            <form className="col-md-8 offset-md-2" onSubmit={handleSubmit}>
+                <h1 className="form-group add-log-header">Add New Activities Log</h1>
                 <div className="form-group">
-                    <label for="location" value={logLocation} onSubmit={location => setLogLocation(location)}>Location</label>
-                    <input type="text" placeholder="Location" id="location" className="form-control"></input>
+                    <label htmlFor="location">Location</label>
+                    <input type="text" placeholder="Enter location" value={logLocation} onChange={e => setLogLocation(e.target.value)} id="location" className="form-control"></input>
                 </div>
                 <div className="form-group">
-                    <label for="date-of-log">Date</label>
+                    <label htmlFor="date-of-log">Date</label>
                     <DatePicker selected={logDate} onChange={date => setLogDate(date)} id="date-of-log" className="form-control" maxDate={new Date()}/>
                 </div>
                 <div className="form-group">
-                    <label for="duration">Time spent at location(minutes)</label>
-                    <input type="number" placeholder ="Enter" id="duration" className="form-control"></input>
+                    <label htmlFor="duration">Time spent at location(minutes)</label>
+                    <input type="number" placeholder ="Enter time spent" id="duration" className="form-control" value={logDuration} onChange={e => setLogDuration(e.target.value)}></input>
                 </div>
                 <div className="form-group">
-                <label for="interactions">Interactions within 6 feet</label>
-                <input type="number" placeholder="Enter" id="interactions" className="form-control"></input>
+                <label htmlFor="interactions">Interactions within 6 feet</label>
+                <input type="number" placeholder="Enter number" id="interactions" className="form-control" value={logInteractions} onChange={e => setLogInteractions(e.target.value)}></input>
                 </div>
                 <button type="submit" className="btn btn-primary">Add Log</button>
             </form>
