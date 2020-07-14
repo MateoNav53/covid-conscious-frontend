f109243a432f8220208085845a3e7823fa2914a1
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-07-13 21:10:24 -0700

    moved state into one useState json

--- src/components/addLog.js
+++ src/components/addLog.js
@@ -1,51 +1,62 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
+import axios from 'axios'
 import "react-datepicker/dist/react-datepicker.css";
 
 function AddLog() {
-    
-    const [logLocation, setLogLocation] = useState('')
-    const [logDate, setLogDate] = useState(null)
-    // const [logItem, setLogItem] = useState()
-    const [logDuration, setLogDuration] = useState('')
-    const [logInteractions, setLogInteractions] = useState('')
-
-    // function logData() {
-    //     console.log(logDate)
-    // }
+    // const [logLocation, setLogLocation] = useState('')
+    // const [logDate, setLogDate] = useState(null)
+    // const [logDuration, setLogDuration] = useState(0)
+    // const [logInteractions, setLogInteractions] = useState(0)
+    const [fullLog, setFullLog] = useState({
+        location: '',
+        logDate: null,
+        duration: 0,
+        interactions: 0
+    })
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        setLogLocation(e.target.value)
-        setLogDate(e.target.value)
-        setLogDuration(e.target.value)
-        setLogInteractions(e.target.value)
-        console.log(logLocation)
-        console.log(logDate)
-        console.log(logDuration)
-        console.log(logInteractions)
+        // setLogLocation(e.target.value)
+        // setLogDate(e.target.value)
+        // setLogDuration(e.target.value)
+        // setLogInteractions(e.target.value)
+        //need to put all this data from inputs into a json
+        setFullLog({
+            location: e.target.value,
+            logDate: e.target.selected,
+            duration: e.target.value,
+            interactions: e.target.value
+        })
+        console.log(fullLog)
+        axios.post('http://localhost:7000/log/add', fullLog)
+            .then(res => console.log(res.data))
         e.target.reset()
     }
+
+    // const createObject() => {
+
+    // }
 
     return(
         <div>
             <form className="col-md-8 offset-md-2" onSubmit={handleSubmit}>
                 <h1 className="form-group add-log-header">Add New Activities Log</h1>
                 <div className="form-group">
-                    <label htmlFor="location">Location</label>
-                    <input type="text" placeholder="Enter location" value={logLocation} onChange={e => setLogLocation(e.target.value)} id="location" className="form-control"></input>
+                    <label htmlFor="log-location">Location</label>
+                    <input type="text" placeholder="Enter location" value={fullLog.location} onChange={e => setFullLog({...fullLog, location: e.target.value})} id="log-location" className="form-control" required></input>
                 </div>
                 <div className="form-group">
                     <label htmlFor="date-of-log">Date</label>
-                    <DatePicker selected={logDate} onChange={date => setLogDate(date)} id="date-of-log" className="form-control" maxDate={new Date()}/>
+                    <DatePicker selected={fullLog.logDate} onChange={date => setFullLog({...fullLog, logDate:date})} id="date-of-log" className="form-control" maxDate={new Date()} required/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="duration">Time spent at location(minutes)</label>
-                    <input type="number" placeholder ="Enter time spent" id="duration" className="form-control" value={logDuration} onChange={e => setLogDuration(e.target.value)}></input>
+                    <input type="number" placeholder ="Enter time spent" id="duration" className="form-control" value={fullLog.duration} onChange={e => setFullLog({...fullLog, duration: e.target.value})} required></input>
                 </div>
                 <div className="form-group">
                 <label htmlFor="interactions">Interactions within 6 feet</label>
-                <input type="number" placeholder="Enter number" id="interactions" className="form-control" value={logInteractions} onChange={e => setLogInteractions(e.target.value)}></input>
+                <input type="number" placeholder="Enter number" id="interactions" className="form-control" value={fullLog.interactions} onChange={e => setFullLog({...fullLog, interactions: e.target.value})} required></input>
                 </div>
                 <button type="submit" className="btn btn-primary">Add Log</button>
             </form>
