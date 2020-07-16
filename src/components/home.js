e1be4dbf620ece2840338a3d0acfbe051fd1d173
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-07-16 14:53:18 -0700

    put in different home page widget

--- src/components/home.js
+++ src/components/home.js
@@ -5,7 +5,7 @@ function Home () {
         <div className="home-container">
             <h1 className="home-item"><strong>Covid Conscious</strong></h1>
             <h3 className="home-item">Keep a track of places been and people seen</h3>
-            <div class="bingwidget offset-xl-6 offset-lg-5 offset-md-4 offset-sm-1" data-type="covid19_stats_map" data-market="en-us" data-language="en-us" data-app="bingwidget"></div>
+            <div class="elfsight-app-4bfe4b25-9e78-4673-836b-f6dfb62163e1"></div>
         </div>
         
         
