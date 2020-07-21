12df937e698c2d1885df2aa470736ec037cfd9fc
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-07-20 17:11:36 -0700

    added log list component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Navbar from './components/navbar';
 import Login from './components/login';
 import Register from './components/register';
 import Home from './components/home';
-import AddLog from './components/addLog'
+import AddLog from './components/addLog';
+import LogList from './components/loglist';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
           <Route path="/addlog" component={AddLog} />
+          <Route path="/loglist" component={LogList} />
         </Switch>
         <div className="App">
           
