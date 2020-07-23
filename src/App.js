1693197bae2320d7ad68ca9cf8dbb3d5d0d95769
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-07-23 11:59:14 -0700

    linked authcontext for user reg/login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Navbar from './components/navbar';
 import Login from './components/login';
 import Register from './components/register';
 import Home from './components/home';
 import AddLog from './components/addLog';
-import LogList from './components/loglist';
+import LogList from './components/LogList';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import {AuthContext} from './context/AuthContext';
 
 function App() {
+  const {user, setUser, isAuthenticated, setIsAuthenticated } = useContext(AuthContext)
+  console.log(user);
+  console.log(isAuthenticated);
   return (
     <HashRouter>
       <Navbar/>
@@ -18,6 +22,8 @@ function App() {
           <Route path="/register" component={Register} />
           <Route path="/addlog" component={AddLog} />
           <Route path="/loglist" component={LogList} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
         </Switch>
         <div className="App">
           
