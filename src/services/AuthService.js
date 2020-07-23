de70fdac9d3252fea9697732a2e3fb371eb004ea
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-07-23 11:57:32 -0700

    edited fetch requests, works now

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,31 +1,33 @@
 export default {
     login: user => {
-        return fetch('user/login',{
+        return fetch('http://localhost:7000/user/login',{
             method: "post",
             body: JSON.stringify(user),
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
             }
         }).then(res => res.json())
         .then(data => data);
     },
     register: user => {
-        return fetch('user/register',{
+        return fetch('http://localhost:7000/user/register',{
             method: "post",
             body: JSON.stringify(user),
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
             }
         }).then(res => res.json())
         .then(data => data);
     },
     logout: () => {
-        return fetch('/user/logoout')
+        return fetch('http://localhost:7000/user/logoout')
             .then(res => res.json())
             .then(data => data);
     },
     isAuthenticated: () => {
-        return fetch('/user/authenticated')
+        return fetch('http://localhost:7000/user/authenticated')
             .then(res=>{
                 if(res.status !== 401)
                     return res.json().then(data => data)
