ac1b4cba9329358c03f53002ece614a38902b070
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-07-27 22:34:31 -0700

    converted fetch requests to axios

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,33 +1,27 @@
+import axios from 'axios'
+
 export default {
     login: user => {
-        return fetch('http://localhost:7000/user/login',{
-            method: "post",
-            body: JSON.stringify(user),
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        }).then(res => res.json())
-        .then(data => data);
+        return axios.post('http://localhost:7000/user/login', user)
+        .then(res => {
+            if(res.status !== 401)
+                    return res.json().then(data => data)
+            else
+                return {isAuthenticated: false, user: {username: ""}};
+        })
     },
     register: user => {
-        return fetch('http://localhost:7000/user/register',{
-            method: "post",
-            body: JSON.stringify(user),
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        }).then(res => res.json())
+        return axios.post('http://localhost:7000/user/register', user)
+        .then(res => res.json())
         .then(data => data);
     },
     logout: () => {
-        return fetch('http://localhost:7000/user/logoout')
+        return axios.get('http://localhost:7000/user/logout')
             .then(res => res.json())
             .then(data => data);
     },
     isAuthenticated: () => {
-        return fetch('http://localhost:7000/user/authenticated')
+        return axios.get('http://localhost:7000/user/authenticated')
             .then(res=>{
                 if(res.status !== 401)
                     return res.json().then(data => data)
