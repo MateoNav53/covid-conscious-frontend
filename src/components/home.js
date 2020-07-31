07fd21b19ea41f6b8a8295da0481c299d8fd2b4d
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-07-30 23:08:36 -0700

    added login and reg buttons

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Home () {
     return(
         <div className="home-container">
             <h1 className="home-item"><strong>Covid Conscious</strong></h1>
-            <h3 className="home-item">Keep a track of places been and people seen</h3>
+            <h4 className="home-item">Keep a log of places been and people seen</h4>
+            <div className="d-flex">
+                <Link to="/login">
+                    <button type="button" className="btn btn-warning home-btn">Login</button>
+                </Link>
+                <Link to="/register">
+                    <button type="button" className="btn btn-warning home-btn">Register</button>
+                </Link>
+
+            </div>
             <div class="elfsight-app-4bfe4b25-9e78-4673-836b-f6dfb62163e1"></div>
         </div>
         
