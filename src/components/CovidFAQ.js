8d571969cd6c18890ef81b95ae04e4a45326867e
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-07-30 17:44:33 -0700

    created initial FAQ messaging

---
+++ src/components/CovidFAQ.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const CovidFaq = () => {
+    return(
+        <div>
+            <p>The Coronavirus(COVID-19) is a potentially fatal respiratory illness that mainly spreads through person-to-person interactions.</p>
+            <p>Respiratory droplets emitted from sneezing, coughing, or just normal talking have the chance to transfer to individuals and infect them</p>
+            <p>The virus is at its highlest likelihood of transmission when people are within 6 feet of each other.</p>
+            <p>The virus can also be spread by people without symptoms as well. Which is why mandatory measures have been put in place for citizens to wear masks in public</p>
+            <p>If contracted, the virus typically incubates within people for 14 days.</p>
+            
+
+            <p>
+            There is no vaccine currently available, so our best chance of minimizing our own infection risk as well as the likelihood of others lies in holding each other accountable
+            Everybody should be staying inside to minimize their chance, but if you have to go out, it's smart keep a log of where you've been and how many people you've come in contact with
+            Covid-Conscious allows you to create and save logs, and keeps track of how many people you've come in contact with in the last 14 days. 
+            When you look back at how many people you've come in contact with in the last two weeks, and multiply that exponentially by how many people each one of those people has come in contact with, the potential for contact tracing is staggering.
+            That's why you need to keep track of your social activity. To know your risk and prevent things from getting out of hand.
+            </p>
+            <p>More information can be found on <a href="https://www.cdc.gov/coronavirus/2019-ncov/faq.html">the Center for Disease Control's website</a> </p>
+        </div>
+    )
+}
+
+export default CovidFaq;
