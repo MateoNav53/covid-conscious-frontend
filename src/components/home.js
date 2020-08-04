3edfd9e903dbc816f40bd801099c63de2c9b3132
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-08-04 14:48:45 -0700

    made auth buttons conditionally render

--- src/components/home.js
+++ src/components/home.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {Link} from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext'
 
 function Home () {
+    const {isAuthenticated} = useContext(AuthContext)
+
+    const authButtons = () => {
+        return (
+        <div className="d-flex">
+            <Link to="/login">
+                <button type="button" className="btn btn-warning home-btn">Login</button>
+            </Link>
+            <Link to="/register">
+                <button type="button" className="btn btn-warning home-btn">Register</button>
+            </Link>
+        </div>
+        )
+    }
+
     return(
         <div className="home-container">
             <h1 className="home-item"><strong>Covid Conscious</strong></h1>
-            <h4 className="home-item">Keep a log of places been and people seen</h4>
+            <h4 className="home-item">Keep a log. Stay on top of your risk</h4>
             <div className="d-flex">
-                <Link to="/login">
-                    <button type="button" className="btn btn-warning home-btn">Login</button>
-                </Link>
-                <Link to="/register">
-                    <button type="button" className="btn btn-warning home-btn">Register</button>
-                </Link>
-
+                {!isAuthenticated ? authButtons() : null}
             </div>
             <div class="elfsight-app-4bfe4b25-9e78-4673-836b-f6dfb62163e1"></div>
         </div>
