80dcb33f5ea7233ae49aba8733b047e199eb01c3
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-08-10 11:52:05 -0700

    added context and link to fetch request. still 401

--- src/components/LogList.js
+++ src/components/LogList.js
@@ -1,40 +1,75 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
+import {AuthContext} from '../context/AuthContext';
+import LogService from '../services/LogService';
 
 function LogList() {
 
     const [logTable, setLogTable] = useState([])
+    const GetContext = useContext(AuthContext)
     let baseInteractions = 0
     const [totalInteractions, setTotalInteractions] = useState(baseInteractions)
 
-    useEffect(() => {
-        axios.get('http://localhost:7000/log/')
-            .then(res => {
-                setLogTable(res.data)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }, [])
+    // useEffect(() => {
+    //     axios.get('http://localhost:7000/user/covidlog')
+    //         // .then(res => {
+    //         //     // GetContext.user.setIsAuthenticated(data.isAuthenticated)
+    //         //     if(res.status !== 401){
+    //         //         return (data => data);
+    //         //     }
+    //         //     else
+    //         //         return {message : {messsagBody : "UnAuthorized", errorMessage : true}};
+    //         // })
+    //         // .catch(err => {
+    //         //     console.log(err)
+    //         // })
+    //         .then(res => {
+    //             if(res.status !== 401) {
+    //                 setLogTable(res.data);
+    //         }})
+    //         .catch((error) => {
+    //             console.log(error);
+    //         })
+    // }, [])
+
+    useEffect(()=>{
+        LogService.getLogs().then(data =>{
+            setLogTable(data.logTable);
+        });
+    },[]);
 
     const deleteLog = (_id, e) => {
         e.preventDefault()
         setLogTable(logTable.filter((selected) => selected._id !== _id))
-        axios.delete('http://localhost:7000/log/'+_id)
+        axios.delete('http://localhost:7000/user/covidlog/'+_id)
             .then(res => console.log(res.data))
     }
 
+    // const counter = () => {
+    //     for(logTable.interactions of logTable) {
+    //         setTotalInteractions(() => {
+    //             logTable.interactions += totalInteractions
+    //             console.log(totalInteractions)
+    //         })
+    //     }
+    // }
+
+    // setTotalInteractions({
+    //     for (let interaction of logTable.interactions) {
+    //         interaction += totalInteractions
+    //     }
+    // })
+    // for(let interaction of logTable.interactions){
+    //     console.log(interaction)
+    // }
+        
+
     return (
         <div>
-            {/* will also need to put users interaction and place count in the last 14 days on here */}
-            {/* Just display all log data here in a table row.
-            Add an edit button that goes to the individual log */}
             <h1 className="text-center">View Log History</h1>
-            <h3 className="offset-md-1">Username</h3>
+            <h3 className="offset-md-1">Hello {GetContext.user.username}</h3>
             <p className="counter offset-md-1">You've come within 6 feet of {totalInteractions} people in the last 14 days</p>
             <table className="table table-hover text-center">
-                {/* Display Username */}
-                {/* Display 14-day count */}
                 <thead className="thead-dark">
                     <tr>
                         <th scope="col">Date</th>
@@ -46,13 +81,8 @@ function LogList() {
                 </thead>
                 <tbody>
                     {
-                        logTable.map(logs => {
-                            
-                            {/* if(logs.interactions){
-                                setTotalInteractions()
-                                logs.interactions+= baseInteractions
-                                console.log(totalInteractions)
-                            } */}
+                        logTable && logTable.map(logs => { 
+                                {/* setTotalInteractions(logs.interactions += baseInteractions) */}
                                 return (
                                     <tr key={logs._id}>
                                         <td>{logs.logDate.substring(0, 10)}</td>
