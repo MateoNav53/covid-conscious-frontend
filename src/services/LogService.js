eb60308612b33c185d402a2186252984d94fe341
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-08-13 14:41:38 -0700

    401 troubleshooting, still not working

--- src/services/LogService.js
+++ src/services/LogService.js
@@ -1,18 +1,25 @@
 export default {
     getLogs: () => {
-        return fetch('http://localhost:7000/user/covidlog', {
+        return fetch('http://localhost:7000/user/covidlog', 
+            {
             headers : { 
               'Content-Type': 'application/json',
-              'Accept': 'application/json'
+              'Accept': 'application/json',
+              'Auhorization': 'Bearer ' + 'jwt'
              }
-      
-          })
-                .then(res =>{
-                    if(res.status !== 401){
-                        return res.json().then(data => data);
-                    }
-                    else
-                        return {message : {messagBody : "UnAuthorized", errorMessage: true}};
-                });
+          }
+            )
+            .then(res =>{
+                console.log(res)
+                if(res.status !== 401){
+                    return res.json().then(data => data);
+                }
+                else
+                    return {message : {messagBody : "UnAuthorized", errorMessage: true}};
+            });
+    },
+
+    postLogs: () => {
+
     }
 }
