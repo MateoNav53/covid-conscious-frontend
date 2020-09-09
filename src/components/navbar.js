90db1a0161c53875c2e718a7fc7b135f21d3840a
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-09-08 22:24:59 -0700

    reformatted router link placement + nav classes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import AuthService from '../services/AuthService';
 import {AuthContext} from '../context/AuthContext';
 
@@ -7,12 +7,11 @@ import {AuthContext} from '../context/AuthContext';
 function Navbar() {
     const {isAuthenticated, user, setIsAuthenticated, setUser} = useContext(AuthContext)
 
-    const logoutUser = (e) => {
+    const logoutUser = () => {
         AuthService.logout().then(data => {
             if(data.success){
                 setUser(data.user);
                 setIsAuthenticated(false);
-                console.log('logout function worked')
             }
         })
     }
@@ -20,61 +19,62 @@ function Navbar() {
     const unauthenticatedNav = () => {
         return(
             <>
-                <Link to='/'>
-                    <li id="nav-id" className="nav-item">
+                <li className="navbar-item">
+                    <Link to='/'>
                         Home
-                    </li>
-                </Link>
-                <Link to='/login'>
-                    <li id="nav-id" className="nav-item">
+                    </Link>
+                </li>
+                <li className="navbar-item">
+                    <Link to='/login'>
                         Login
-                    </li>
-                </Link>
-                <Link to='/register'>
-                    <li id="nav-id" className="nav-item">
+                    </Link>
+                </li>
+                <li className="navbar-item">
+                    <Link to='/register'>
                         Register
-                    </li>
-                </Link>
-                <Link to='/covidfaq'>
-                    <li id="nav-id" className="nav-item">
+                    </Link>
+                </li>
+            
+                <li className="navbar-item">
+                    <Link to='/covidfaq'>
                         COVID FAQ
-                    </li>
-                </Link>
+                    </Link>
+                </li>
             </>
         )
     }
     const authenticatedNav = () => {
         return(
             <>
-                <Link to='/'>
-                    <li id="nav-id" className="nav-item">
+                <li className="navbar-item">
+                    <Link to='/'>
                         Home
-                    </li>
-                </Link>
-                <Link to='/addlog'>
-                    <li id="nav-id" className="nav-item">
+                    </Link>
+                </li>
+                <li className="navbar-item">
+                    <Link to='/addlog'>
                         Add Log
-                    </li>
-                </Link>
-                <Link to='/loglist'>
-                    <li id="nav-id" className="nav-item">
-                        View Logs
-                    </li>
-                </Link>
-                <Link to='/covidfaq'>
-                    <li id="nav-id" className="nav-item">
+                    </Link>
+                </li>
+                <li className="navbar-item">
+                    <Link to='/loglist'>
+                        View {user.username}'s Logs
+                    </Link>
+                </li>
+                <li className="navbar-item">
+                    <Link to='/covidfaq'>
                         COVID FAQ
-                    </li>
-                </Link>
+                    </Link>
+                </li>
                 <Link to='/'>
-                    <button type="button" className="btn btn-secondary btn-link nav-item" onClick={logoutUser}>Logout</button>
+                    <button type="button" className="btn btn-secondary" onClick={logoutUser}>Logout</button>
                 </Link>
             </>
         )
     }
     return (
-        <nav className="navbar navbar-expand-sm navbar-light bg-primary">
-                <a href="/" className="brand">               
+        <nav className="navbar navbar-expand-md navbar-dark bg-primary sticky-top">
+                <a href="/" className="brand mb-auto">               
                     Covid Conscious  
                 </a>
             <ul className="nav-links navbar-nav navbar-brand">
