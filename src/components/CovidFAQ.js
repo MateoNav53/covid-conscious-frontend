a5a37c3bb3570a317b68e6d40c659280dfa6209c
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-09-08 22:06:15 -0700

    rewording paragraphs

--- src/components/CovidFAQ.js
+++ src/components/CovidFAQ.js
@@ -5,7 +5,7 @@ const CovidFaq = () => {
         <div className="col-md-10 offset-md-1">
             <h2 className="header">About COVID-19</h2>
             <p>The Coronavirus(COVID-19) is a devastating respiratory illness with fatal potential. 
-            It that mainly spreads through respiratory droplets which can be exchanged through close person-to-person interactions.
+            It mainly spreads through respiratory droplets which can be exchanged through close person-to-person interactions.
             Actions such as sneezing, coughing, or just normal talking have the chance to transfer to individuals and infect them</p>
 
             <p>The virus is at its highlest likelihood of transmission when people are within 6 feet of each other.
@@ -17,12 +17,12 @@ const CovidFaq = () => {
             Outside of wearing a mask, the best thing we can do to combat the pandemic is be cognizant of our behavior and encourage others to do the same.
             We need to work in unison to minimize the risk keep us, our loved ones, and the strangers around us free from Coronavirus.</p>
             <p>Everybody should be staying inside to minimize their chance, but if you have to go out, it's smart keep a log of where you've been and how many people you've come in contact with.
-             That way, if you find out later that someone contracted Covid at a location you logged in the last 14 days, you know that you need to get yourself tested. 
+             That way, if you find out later that someone contracted Covid at a location you logged in the last 14 days, or you were the one to catch it, you know who to contact and that you need to get yourself tested. 
             Covid-Conscious allows you to create and save logs, and keeps track of how many people you've come in contact with in the last 14 days. 
             When you look back at how many people you've come in contact with in the last two weeks, and multiply that exponentially by how many people each one of those people has come in contact with, the potential for contact tracing is staggering.
             That's why you need to keep track of your social activity. To know your risk and prevent things from getting out of hand.
             </p>
-            <p>More COVID-19 information can be found on <a href="https://www.cdc.gov/coronavirus/2019-ncov/faq.html">The Center for Disease Control's website</a> </p>
+            <p>More COVID-19 information can be found on <a href="https://www.cdc.gov/coronavirus/2019-ncov/faq.html" id="referral" className="faq-link">The Center for Disease Control's website</a> </p>
         </div>
     )
 }
