0671106d794ef8970d196b1b9e8a94ba8f568428
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-09-17 13:02:11 -0700

    removed comments and unnecessary state variable

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -6,20 +6,16 @@ export const AuthContext = createContext();
 export default ({ children }) => {
     const [user, setUser] = useState(null);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
-    // const [isLoaded, setIsLoaded] = useState(false);
 
     useEffect(() => {
         AuthService.isAuthenticated().then(data => {
             setUser(data.user)
             setIsAuthenticated(data.isAuthenticated)
-            // setIsLoaded(true);
         });
     }, []);
 
     return (
         <div>
-            {/* !isLoaded ? <h1>Application Loading...</h1> :  */}
-            {/* The Provider followed by defining the value sets what we want our global state to be */}
             <AuthContext.Provider value={{user, setUser, isAuthenticated, setIsAuthenticated}}>
                 {children}
             </AuthContext.Provider>
