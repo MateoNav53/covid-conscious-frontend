2d9c49d2d87a1413fba448599239d5280528cb5c
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-09-23 17:33:02 -0700

    Updates

---
+++ precache-manifest.e44dfed6e4a22b2dacff5a3781ff8148.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ba76d75a9eea822bf23ccbf5ff1706",
+    "url": "/covid-conscious-frontend/index.html"
+  },
+  {
+    "revision": "9c96558103eb6748c01d",
+    "url": "/covid-conscious-frontend/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "3539ebe0f747bf8eb3b7",
+    "url": "/covid-conscious-frontend/static/css/main.04be3985.chunk.css"
+  },
+  {
+    "revision": "9c96558103eb6748c01d",
+    "url": "/covid-conscious-frontend/static/js/2.f59de779.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/covid-conscious-frontend/static/js/2.f59de779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3539ebe0f747bf8eb3b7",
+    "url": "/covid-conscious-frontend/static/js/main.f363a9fc.chunk.js"
+  },
+  {
+    "revision": "f825f55aaa9e7c604ae5",
+    "url": "/covid-conscious-frontend/static/js/runtime-main.68105f6c.js"
+  }
+]);
