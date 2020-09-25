b403e1f8c25a92fd16f8a0aa0c4622f8b090b065
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-09-24 17:05:24 -0700

    restructured login function

--- src/components/login.js
+++ src/components/login.js
@@ -11,14 +11,14 @@ function Login(props) {
     const authContext = useContext(AuthContext);
     const [error, setError] = useState('')
 
+    const onChange = (e) => {
+        setUser({
+            ...user, [e.target.name]: e.target.value
+        });
+    }
+
     const submitLogin = (e) => {
         e.preventDefault()
-        const newValueStr = e.target.value;
-        console.log('newvaluestr', newValueStr);
-        setUser({
-            username: newValueStr,
-            password: newValueStr 
-        });
         AuthService.login(user).then(data=>{
             console.log(data)
             const {isAuthenticated, user} = data;
@@ -41,11 +41,11 @@ function Login(props) {
                 <h1 className="form-group page-header">Login</h1>
                 <div className="form-group">
                     <label htmlFor="username">Username</label>
-                    <input type="text" placeholder="Enter username" value={user.username||""} name="username" onChange={e => setUser({...user, username: e.target.value})} id="username" className="form-control" required></input>
+                    <input type="text" placeholder="Enter username" name="username" onChange={onChange} id="username" className="form-control" required></input>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input type="password" placeholder="Enter password" value={user.password||""} name="password" onChange={e => setUser({...user, password: e.target.value})} id="password" className="form-control" required></input>
+                    <input type="password" placeholder="Enter password" name="password" onChange={onChange} id="password" className="form-control" required></input>
                 </div>
                 <button type="submit" className="btn btn-warning">Login</button>
                 {error ? <div className="alert alert-danger">{error}</div> : null}
