3ed71cfbe6a9b9016b6718cbe1caab60327ca619
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2020-10-06 14:25:01 -0700

    form submission code cleanup

--- src/components/register.js
+++ src/components/register.js
@@ -27,14 +27,12 @@ function Register(props) {
         });
     }
 
+    const onChange = (e) => {
+        setUser({...user, [e.target.name]: e.target.value})
+    }
+
     const submitReg = (e) => {
         e.preventDefault()
-        setUser({
-            username: e.target.value,
-            fullname: e.target.value,
-            email: e.target.value,
-            password: e.target.value,
-        });
         AuthService.register(user).then(data=>{
             const { message } = data;
             setMessage(message);
@@ -42,7 +40,7 @@ function Register(props) {
             if(!message.errorMessage){
                 timerID = setTimeout(()=>{
                 props.history.push('/login');
-            },2500)
+            },1500)
             }
         }) 
     }
@@ -53,19 +51,19 @@ function Register(props) {
                 <h1 className="form-group page-header">Register</h1>
                 <div className="form-group">
                     <label htmlFor="username">Username</label>
-                    <input type="text" placeholder="Enter username" value={user.username||""} onChange={e => setUser({...user, username: e.target.value})} name="username" id="username" className="form-control" required></input>
+                    <input type="text" placeholder="Enter username" value={user.username||""} onChange={onChange} name="username" id="username" className="form-control" required></input>
                 </div>
                 <div className="form-group">
                     <label htmlFor="fullname">Full Name</label>
-                    <input type="text" placeholder="Enter full name" value={user.fullname||""} onChange={e => setUser({...user, fullname: e.target.value})} name="fullname" id="full-name" className="form-control" required></input>
+                    <input type="text" placeholder="Enter full name" value={user.fullname||""} onChange={onChange} name="fullname" id="full-name" className="form-control" required></input>
                 </div>
                 <div className="form-group">
                     <label htmlFor="email">Email</label>
-                    <input type="email" placeholder="Enter email" value={user.email||""} onChange={e => setUser({...user, email: e.target.value})} name="email" id="email" className="form-control" required></input>
+                    <input type="email" placeholder="Enter email" value={user.email||""} onChange={onChange} name="email" id="email" className="form-control" required></input>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input type="password" placeholder="Enter password" value={user.password||""} onChange={e => setUser({...user, password: e.target.value})} name="password" id="password" className="form-control" required></input>
+                    <input type="password" placeholder="Enter password" value={user.password||""} onChange={onChange} name="password" id="password" className="form-control" required></input>
                 </div>
                 <button type="submit" className="btn btn-warning">Register</button>
             </form>
